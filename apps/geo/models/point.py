aa41b5f2f7ff89be5280c8bd56e29cdc32539d29
parkhomin <aparkhomin@sberbank.ru>
2022-03-12 03:14:14 +0300

    изменил поля

--- apps/geo/models/point.py
+++ apps/geo/models/point.py
@@ -5,8 +5,8 @@ class Point(models.Model):
     """Точка переработки"""
 
     address = models.CharField(max_length=128, verbose_name="Адрес")
-    longitude = models.CharField(max_length=128, verbose_name="Долгота")
-    latitude = models.CharField(max_length=128, verbose_name="Широта")
+    lng = models.CharField(max_length=128, verbose_name="Долгота")
+    lat = models.CharField(max_length=128, verbose_name="Широта")
     description = models.CharField(max_length=255, verbose_name="Описание")
 
     @property
